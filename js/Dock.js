5d98760f59fc0b84a3ced8a3dfa6913eb489c2fd
Camden Rudisill <camden.rudisill@richmond.edu>
2015-06-29 18:42:50 -0400

    titles to js

--- js/Dock.js
+++ js/Dock.js
@@ -5,17 +5,12 @@
 
 var elements = document.getElementsByClassName('dock-item');
 var label = document.getElementsByClassName('label');
-var titleDiv = document.createElement("span");
-//var triangle = document.createElpent("div");
+var title = document.createElement("span");
 var codeSnippet = document.getElementById("code");
 var js = document.getElementById("js");
 var arrayOfIds = [];
 js.style.cursor = "pointer";
 var title;
-
-// assign id to enable easier editing in custom.css
-//titleDiv.className = "title-div";
-//triangle.id = "triangle-div";
 
 
 // store each dock icon's id in an array
@@ -24,7 +19,6 @@ for (var i = 0; i < elements.length; i++) {
 	arrayOfIds.push(elements[i].id);
 
 }
-console.log(arrayOfIds);
 
 
 // iterate over the array of ids to add title on hover
@@ -33,19 +27,12 @@ for (var i = 0; i < arrayOfIds.length; i++) {
 	// add title-div when mouse over
 	elements[i].addEventListener('mouseover', (function(i) {
 		return function() {
-			console.log(title);
-			titleDiv.innerHTML = arrayOfIds[i];
-			label[i].appendChild(titleDiv);
+			title.innerHTML = arrayOfIds[i];
+			label[i].appendChild(title);
 
 		};
 	})(i), false);
 
-	// rpove title-div when mouse out				
-	label[i].addEventListener('mouseout', (function(i) {
-		return function() {
-			elements[i].removeChild(title);
-		};
-	})(i), false);
 }
 
 function showCode() {
